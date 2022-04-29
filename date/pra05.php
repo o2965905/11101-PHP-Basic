a938ca38c73482f9c70a3232b7715a7ecb4eca37
o2965905 <o2965905@gmail.com>
2022-04-29 15:31:47 +0800

    老師版本

--- date/pra05.php
+++ date/pra05.php
@@ -1,6 +1,3 @@
-<h2>使用陣列來做月曆</h2>
-<!-- 先用陣列計算出需要多少格數 -->
-<!-- 再來製作出表格 -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,91 +8,160 @@
     <style>
         table{
             border-collapse: collapse;
-            font-family: '標楷體';
         }
+
         table td{
-            padding: 20px;
+            padding:5px;
             text-align: center;
-            border: 1px solid lightgrey;
+            border:1px solid #aaa;
         }
-        .top{
-            background-color: lightgoldenrodyellow;
-            font-weight: bold;
+        .weekend{
+            background:pink;
+        }
+        .workday{
+            background:white;
+        }
+        .today{
+            background:lightseagreen;
         }
     </style>
 </head>
 <body>
-    <?php
-    $month=5;
+    <h1>使用陣列來做月曆</h1>
+<?php
+$month=2;
+
+?>
+<table>
+    <tr>
+        <td>日</td>
+        <td>一</td>
+        <td>二</td>
+        <td>三</td>
+        <td>四</td>
+        <td>五</td>
+        <td>六</td>
+    </tr>
+<?php
+
+$firstDay=date("Y-").$month."-1";
+$firstWeekday=date("w",strtotime($firstDay));
+$monthDays=date("t",strtotime($firstDay));
+$lastDay=date("Y-").$month."-".$monthDays;
+$today=date("Y-m-d");
+$lastWeekday=date("w",strtotime($lastDay));
+$dateHouse=[];
+
+for($i=0;$i<$firstWeekday;$i++){
+    $dateHouse[]="";
+}
+
+for($i=0;$i<$monthDays;$i++){
+    $date=date("Y-m-d",strtotime("+$i days",strtotime($firstDay)));
+    $dateHouse[]=$date;
+}
+
+for($i=0;$i<(6-$lastWeekday);$i++){
+    $dateHouse[]="";
+}
+
+echo "<pre>";
+print_r($dateHouse);
+echo "</pre>";
+
+foreach($dateHouse as $key=>$day){
+
+    if($key%7==0){
+        echo "<tr>";
+    }
+
+    if(!empty($day)){
+        $dayFormat=date("d",strtotime($day));
+    }else{
+        $dayFormat="";
+    }
+
+    //$dayFormat=(!empty($day))?date("d",strtotime($day)):"";
+
+    echo "<td>{$dayFormat}</td>";
+
+    if($key%7==6){
+        echo "</tr>";
+    }
     
+}
 
 
-    ?>
-    <table>
-        <tr>
-            <td class="top">日</td>
-            <td class="top">一</td>
-            <td class="top">二</td>
-            <td class="top">三</td>
-            <td class="top">四</td>
-            <td class="top">五</td>
-            <td class="top">六</td>
-        </tr>
-        <?php 
+?>
+</table>
+<hr>
+<?php
+echo "月份".$month;
+echo "<br>";
+echo "第一天是".$firstDay;
+echo "<br>";
+echo "第一天是星期".$firstWeekday;
+echo "<br>";
+echo "最後一天是".$lastDay;
+echo "<br>";
+echo "當月天數共".$monthDays;
+echo "<br>";
+?>
+
+
+<table>
+    <tr>
+        <td>日</td>
+        <td>一</td>
+        <td>二</td>
+        <td>三</td>
+        <td>四</td>
+        <td>五</td>
+        <td>六</td>
+    </tr>
+<?php
+
+for($i=0;$i<6;$i++){
+    echo "<tr>";
+    
+    for($j=0;$j<7;$j++){
+        $d=$i*7+($j+1)-$firstWeekday-1;
         
-        $firstDay=date("Y-").$month."-1";
-        $firstWeekday=date("w",strtotime($firstDay));
-        $monthDays=date("t",strtotime($firstDay));
-        $lastDay=date("Y-").$month."-".$monthDays;
-        $today=date("Y-m-d");
-
-        /* 先設定一個空陣列 */
-        /* 再用for迴圈去運算出當月所有日期 */
-        $dateHouse=[];
-        for($i=0;$i<$monthDays;$i++){
-            $date=date("Y-m-d",strtotime("+$i days",strtotime($firstDay)));
-            $dateHouse[]=$date;
-        }
-        
-        echo "<pre>";
-        print_r($dateHouse);
-        echo "</pre>";
-
-        echo $month."月份";
-        echo "<br>";
-        echo "第一天是".$firstDay;
-        echo "<br>";
-        echo "第一天是星期".$firstWeekday;
-        echo "<br>";
-        echo "最後一天是".$lastDay;
-        echo "<br>";
-        echo "當月天數共".$monthDays."天";
-        echo "<br>";
-
-        for($i=0;$i<6;$i++){
-            echo "<tr>";
-            for($j=0;$j<7;$j++){
-                $d=$i*7+($j+1)-$firstWeekday-1;
-                
-                if($d>=0 && $d<$monthDays){ 
-                    $fs=strtotime($firstDay);
-                    $shiftd=strtotime("+$d days",$fs);
-                    $date=date("j",$shiftd);
-                    $w=date("w",$shiftd);
-                    if($w==0 || $w==6){
-                        echo "<td style='color:red'>";
-                    }else{
-                        echo "<td>";
-                    }
-                    echo $date;
-                    echo "</td>";
-                }else{
-                    echo "<td></td>";
-                }
+        if($d>=0 && $d<$monthDays){
+            $fs=strtotime($firstDay);
+            $shiftd=strtotime("+$d days",$fs);
+            $date=date("d",$shiftd);
+            $w=date("w",$shiftd);
+            $chktoday="";
+            if(date("Y-m-d",$shiftd)==$today){
+                $chktoday='today';
             }
-            echo "</tr>";
+            //$date=date("Y-m-d",strtotime("+$d days",strtotime($firstDay)));
+            if($w==0 || $w==6){
+                echo "<td class='weekend $chktoday' >";
+            }else{
+                echo "<td class='workday $chktoday'>";
+            }
+            echo $date;
+            echo "</td>";
+        }else{
+            echo "<td></td>";
         }
-        ?>
-    </table>
+            
+
+    }
+
+    echo "</tr>";
+}
+
+
+
+?>
+
+
+</table>
+
+
+
 </body>
 </html>
